0e5c4bf1418472b065f1103150e0221f77f382df
macn <macn@itu.dk>
2023-02-23 11:28:38 +0100

    adding count

--- localStorage.js
+++ localStorage.js
@@ -1,7 +1,9 @@
-function storeThing() {
+function store() {
   // get user input
   let input = document.getElementById("userInput");
-
   // save user input
   localStorage.setItem("userInput", input.value);
+  // saving initial count
+  let count = 0;
+  localStorgage.setItem("count", count);
 }
